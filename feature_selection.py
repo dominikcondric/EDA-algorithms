18f86321d7b0ebe1c8d25441c216a4fca1406a53
dominikcondric <domi.condric107@gmail.com>
2022-06-28 19:03:56 +0200

    Added timer

--- feature_selection.py
+++ feature_selection.py
@@ -9,6 +9,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+import time
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -39,11 +40,11 @@ datasets = [
 classifiers = [
     ("KNeighborsClassifier", KNeighborsClassifier(15)),
     ("SVC", SVC()),
-    ("DecisionTreeClassifier", DecisionTreeClassifier(max_depth=5)),
-    ("RandomForestClassifier", RandomForestClassifier(max_depth=5, max_features=20)),
-    ("AdaBoostClassifier", AdaBoostClassifier()),
+    # ("DecisionTreeClassifier", DecisionTreeClassifier(max_depth=5)),
+    # ("RandomForestClassifier", RandomForestClassifier(max_depth=5, max_features=20)),
+    # ("AdaBoostClassifier", AdaBoostClassifier()),
     ("GaussianNB", GaussianNB()),
-    ("QuadraticDiscriminantAnalysis", QuadraticDiscriminantAnalysis()),
+    # ("QuadraticDiscriminantAnalysis", QuadraticDiscriminantAnalysis()),
 ]
 
 def plot_fitness(axs, no_fs_score, umda_fs_score, bmda_fs_score, boa_fs_score):
@@ -73,6 +74,10 @@ def classify(features):
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=42)
     current_classifier.fit(X_train, y_train)
     return current_classifier.score(X_test, y_test)
+
+def get_float_string(value, decimals_count=4):
+    format_string = "{:." + str(decimals_count) + "f}"
+    return f"{format_string}".format(value)
 
 umda = UMDA(
     classify,
@@ -130,24 +135,38 @@ def main():
                 
             current_classifier = classifier
             current_data = data 
+
+            start_time = time.perf_counter()
             no_fs_score = classify([1 for _ in range(X.shape[1])])
+            no_fs_time = time.perf_counter() - start_time
 
             max_features = X.shape[1] / 2
+            start_time = time.perf_counter()
             umda_best = umda.calculate(X.shape[1])[-1]
+            umda_time = time.perf_counter() - start_time
             umda_fs_score = umda_best.fitness
             umda_feature_count = umda_best.bitstring.count(1) # Calculating number of features
 
+            start_time = time.perf_counter()
             bmda_best = bmda.calculate(X.shape[1])[-1]
             bmda_fs_score = bmda_best.fitness
+            bmda_time = time.perf_counter() - start_time
             bmda_feature_count = bmda_best.bitstring.count(1) # Calculating number of features
 
+            start_time = time.perf_counter()
             boa_best = boa.calculate(X.shape[1])[-1]
+            boa_time = time.perf_counter() - start_time
             boa_fs_score = boa_best.fitness
             boa_feature_count = boa_best.bitstring.count(1) # Calculating number of features
 
             plot_fitness(axs_fitness[i][j], no_fs_score, umda_fs_score, bmda_fs_score, boa_fs_score)
             plot_features(axs_features[i][j], X.shape[1], umda_feature_count, bmda_feature_count, boa_feature_count)
-            print(f"\t{cl_name}:{' ' * (30 - len(cl_name))}NO FS: {no_fs_score}, UMDA FS: {umda_fs_score}, BMDA FS: {bmda_fs_score}, BOA FS: {boa_fs_score}")
+            print(f"\t{cl_name}:{' ' * (30 - len(cl_name))}",
+                f"NO FS: {get_float_string(no_fs_score)} ({get_float_string(no_fs_time)}s), ",
+                f"UMDA FS: {get_float_string(umda_fs_score)} ({get_float_string(umda_time)}s), "
+                f"BMDA FS: {get_float_string(bmda_fs_score)} ({get_float_string(bmda_time)}s), "
+                f"BOA FS: {get_float_string(boa_fs_score)} ({get_float_string(boa_time)}s)")
+
             j += 1
         i += 1
         print()
